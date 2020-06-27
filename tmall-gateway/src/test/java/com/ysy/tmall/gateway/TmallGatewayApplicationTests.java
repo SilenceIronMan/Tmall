39a9c9310035c4cd98df6c2a7c0c311d3434cf65
Yinshiyu <yinshiyu_2008@126.com>
2020-06-27 21:20:17 +0800

    springcloud gateway API网关

---
+++ tmall-gateway/src/test/java/com/ysy/tmall/gateway/TmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.tmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
