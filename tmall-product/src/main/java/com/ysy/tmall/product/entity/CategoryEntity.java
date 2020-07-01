f9e240b31fd43089b95f896fa39b9644c44d53c2
Yinshiyu <yinshiyu_2008@126.com>
2020-07-02 02:11:08 +0800

    JSR303 valid  以及 hibernate Validated ,自定义校验器的使用

--- tmall-product/src/main/java/com/ysy/tmall/product/entity/CategoryEntity.java
+++ tmall-product/src/main/java/com/ysy/tmall/product/entity/CategoryEntity.java
@@ -4,12 +4,10 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
-
-import lombok.Data;
 
 /**
  * 商品三级分类
