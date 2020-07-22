09774102b4644e160eddbdc277f8bd42264c90c9
Yinshiyu <yinshiyu_2008@126.com>
2020-07-22 22:09:02 +0800

    WebMvcConfigurer 配置controller 以及 视图

---
+++ tmall-auth-server/src/main/java/com/ysy/tmall/tmallauthserver/config/MyWebConfig.java
@@ -0,0 +1,18 @@
+package com.ysy.tmall.tmallauthserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @anthor silenceYin
+ * @date 2020/7/22 - 21:59
+ */
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+}
---
+++ tmall-auth-server/src/main/java/com/ysy/tmall/tmallauthserver/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.ysy.tmall.tmallauthserver.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @anthor silenceYin
+ * @date 2020/7/22 - 21:19
+ */
+@Controller
+public class LoginController {
+
+//    @GetMapping(value = "/login.html")
+//    public String loginPage() {
+//
+//        return "login";
+//    }
+//
+//    @GetMapping(value = "reg.html")
+//    public String regPage() {
+//
+//        return "reg";
+//    }
+}
