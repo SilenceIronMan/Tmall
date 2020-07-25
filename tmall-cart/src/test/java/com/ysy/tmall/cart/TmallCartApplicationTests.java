d138cabb1d2f4304b8daf80ae0fce0f67b4eadad
Yinshiyu <yinshiyu_2008@126.com>
2020-07-26 02:28:19 +0800

    商城购物车模块初步搭建

---
+++ tmall-cart/src/test/java/com/ysy/tmall/cart/TmallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.tmall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
