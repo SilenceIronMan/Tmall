680deb7b1c8ff4df8be322f1ac9c0d518bea159c
Yinshiyu <yinshiyu_2008@126.com>
2020-08-15 22:50:04 +0800

    feat:下订单错误信息确认页面显示

--- tmall-order/src/main/java/com/ysy/tmall/order/web/OrderWebController.java
+++ tmall-order/src/main/java/com/ysy/tmall/order/web/OrderWebController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.concurrent.ExecutionException;
 
@@ -48,7 +49,7 @@ public class OrderWebController {
      * @return
      */
     @PostMapping("/submitOrder")
-    public String submitOrder(OrderSubmitVo vo, Model model) {
+    public String submitOrder(OrderSubmitVo vo, Model model, RedirectAttributes redirectAttributes) {
         log.info("下订单传递数据" + vo);
         SubmitOrderResponseVo responseVo = orderService.submitOrder(vo);
         // 创建订单 验证令牌 验证价格 锁库存
@@ -58,6 +59,19 @@ public class OrderWebController {
             model.addAttribute("submitOrderResp", responseVo);
             return "pay";
         } else {
+            String msg = "下单失败；";
+            switch (responseVo.getCode()) {
+                case 1:
+                    msg += "订单信息过期，请刷新再次提交";
+                    break;
+                case 2:
+                    msg += "订单商品价格发生变化，请确认后再次提交";
+                    break;
+                case 3:
+                    msg += "库存锁定失败，商品库存不足";
+                    break;
+            }
+            redirectAttributes.addFlashAttribute("msg", msg);
             return "redirect:http://order.ysymall.com/toTrade";
         }
 
