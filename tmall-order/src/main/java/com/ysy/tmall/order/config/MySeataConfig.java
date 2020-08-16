aa71a401323dc330a8c093139ced1794bfbdd667
Yinshiyu <yinshiyu_2008@126.com>
2020-08-17 03:04:11 +0800

    Seata 功能引入

--- tmall-order/src/main/java/com/ysy/tmall/order/config/MySeataConfig.java
+++ tmall-order/src/main/java/com/ysy/tmall/order/config/MySeataConfig.java
@@ -1,14 +1,9 @@
 package com.ysy.tmall.order.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
@@ -22,21 +17,7 @@ public class MySeataConfig {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource druidDataSource() {
-        return new DruidDataSource();
-    }
-
-    @Bean
-    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
-        return new DataSourceProxy(dataSource);
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSourceProxy)throws Exception{
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
-                .getResources("classpath*:/mapper/**/*.xml"));
-        sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
-        return sqlSessionFactoryBean.getObject();
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
     }
 }
