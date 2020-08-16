022106696dfb8a18bc2f4613e1e5df6c1a126b23
Yinshiyu <yinshiyu_2008@126.com>
2020-08-17 02:53:32 +0800

    Seata 功能引入

---
+++ tmall-order/src/main/java/com/ysy/tmall/order/config/MyMQConfig.java
@@ -0,0 +1,69 @@
+package com.ysy.tmall.order.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 延时队列  定时关单
+ * @anthor silenceYin
+ * @date 2020/8/17 - 2:01
+ */
+@Configuration
+public class MyMQConfig {
+    // @Bean Binding Queue Exchange
+    //延时队列  死信队列
+
+    /**
+     * 容器中 Binding Queue Exchange  会自动在rabbitMq中创建(RabbitMq中不存在 该 Binding Queue Exchange 时)
+     * @return
+     */
+    @Bean
+    public Queue orderDelayQueue(){
+        //Queue(String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments)
+
+        Map<String, Object> arguments = new HashMap<>();
+        //死信路由
+        arguments.put("x-dead-letter-exchange", "order-event-exchange");
+        //死信路由键
+        arguments.put("x-dead-letter-routing-key", "order.release.order");
+        //过期时间
+        arguments.put("x-message-ttl", 60000);
+        Queue queue = new Queue("order.delay.queue", true, false, false, arguments);
+        return queue;
+    }
+
+    @Bean
+    public Queue orderReleseOrderQueue(){
+        Queue queue = new Queue("order.release.order.queue", true, false, false);
+        return queue;
+    }
+
+    //交换机
+    @Bean
+    public Exchange orderEventExchange(){
+        TopicExchange topicExchange = new TopicExchange("order-event-exchange", true, false);
+        return topicExchange;
+    }
+
+    //订单的binding
+    @Bean
+    public Binding ordeCreateOrderBinding() {
+        Binding binding = new Binding("order.delay.queue", Binding.DestinationType.QUEUE, "order-event-exchange", "order.create.order", null);
+        return binding;
+    }
+
+    //binding
+    @Bean
+    public Binding ordeReleaseOrderBinding() {
+        Binding binding = new Binding("order.release.order.queue", Binding.DestinationType.QUEUE, "order-event-exchange", "order.release.order", null);
+        return binding;
+    }
+
+}
