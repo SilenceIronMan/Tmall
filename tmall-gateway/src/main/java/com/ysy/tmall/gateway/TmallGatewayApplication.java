2d5ba08316e38fad2b2c66babec30de2c56fff97
Yinshiyu <yinshiyu_2008@126.com>
2020-09-20 02:14:10 +0800

    feat(all): 引入sentinel 网关控制

--- tmall-gateway/src/main/java/com/ysy/tmall/gateway/TmallGatewayApplication.java
+++ tmall-gateway/src/main/java/com/ysy/tmall/gateway/TmallGatewayApplication.java
@@ -2,9 +2,10 @@ package com.ysy.tmall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class TmallGatewayApplication {
 
